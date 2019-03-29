cd5a1d58e2b10105420730b2c7d38d10cf72b65d
liutong-cnu <1511767012@qq.com>
2019-03-29 15:52:08 +0800

    Delete temp.py

--- 新词发现/temp.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#encoding=utf8
-'''
-  Author: liutong liutong@cmos.chinamobile.com
-  create@2019-03-20 16:48:20
-'''
-with open("res") as f:
-    for line in f.readlines():
-        print(line.split()[0])
